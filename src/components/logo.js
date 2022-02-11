120deab7ab5b2b084d143c1fed31bc273bb18308
Prakhar Asaiya <63089922+prakhar-asaiya@users.noreply.github.com>
2022-02-11 13:18:33 +0530

    Minor Changes

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,20 +1,20 @@
 /** @jsx jsx */
-import { jsx, Image } from 'theme-ui';
-import { Link } from 'components/link';
+import { jsx, Image } from "theme-ui";
+import { Link } from "components/link";
 
 export default function Logo({ src, ...rest }) {
   return (
     <Link
-    path="/"
-    sx={{
-      variant: 'links.logo',
-      display: 'flex',
-      cursor: 'pointer',
-      mr:15,
-    }}
-    {...rest}
+      path="/"
+      sx={{
+        variant: "links.logo",
+        display: "flex",
+        cursor: "pointer",
+        mr: 15,
+      }}
+      {...rest}
     >
-      <Image src={src} alt="NextJs Landing Page Logo"/>
+      <Image src={src} alt="StartUp Landing Page Logo" />
     </Link>
   );
 }
